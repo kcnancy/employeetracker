485b7bad87c71cb0f9d21b64f3b90d2933b665fd
Nancy McBride <njmcbride224@gmail.com>
2021-03-10 21:54:00 -0600

    fixed data in seed file and fixed query syntax.

--- db/query.js
+++ db/query.js
@@ -3,15 +3,20 @@ module.exports = {
 
     findAllEmployees: ()=> {
     let query =
-    'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.department_name AS department, CONCAT(manager.first_name, manager.last_name) AS manager'
-    query =+ "FROM employee"
-    query =+ "ON employee.role_id = role.role_id;"
-    query += "LEFT JOIN department ON department.id = role.department_id"
-    query += "LEFT JOIN employee manager ON (manager.id = employee.manager_id)"
-    query =+ "ORDER BY employee.id;"
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    LEFT JOIN role ON (employee.role_id = role.id)
+    LEFT JOIN department ON (role.department_id = department.id)
+    ORDER BY employee.id`;
+   
     connection.query (query,(err, res)=>{
-        if (err) throw err
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW ALL EMPLOYEES');
+        console.log('\n');
         console.table(res);
+        //prompt();
 
     })
     }
